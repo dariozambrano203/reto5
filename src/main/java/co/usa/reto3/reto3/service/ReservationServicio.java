9aaba0caa775bf24bde4d71240577782f67f725f
dgzambranoc <dario.zambrano.mt@correo.usa.edu.co>
2021-12-15 10:48:20 -0500

    reto5

--- src/main/java/co/usa/reto3/reto3/service/ReservationServicio.java
+++ src/main/java/co/usa/reto3/reto3/service/ReservationServicio.java
@@ -16,18 +16,35 @@ import co.usa.reto3.reto3.reportes.StatusReservas;
 import co.usa.reto3.reto3.repository.ReservationRepositorio;
 
 @Service
+/**
+ * Reservacionservicio implementa metodos para hacer reservas 
+ * @author Dario Zambrano
+ */
 public class ReservationServicio {
     @Autowired
+    /**
+    * An crea una variable nombre crud tipo repositorio.
+    */
     private ReservationRepositorio crud;
 
+    /**
+    * consigue toda la lista de reservas
+    */
+
     public List<Reservation>getAll(){
+
         return crud.getAll();
     }
-
+/**
+    * busca el id de la reserva
+    */
     public Optional<Reservation>getReservation(int reservationId){
         return crud.getReservation(reservationId);
     }
 
+/**
+    * Apara guardar la reserva @return reserva
+    */
     public Reservation save(Reservation reservation){
         //Verificamos si el Departamento es nuevo y de ser así se guarda
         if (reservation.getIdReservation()==null) {
@@ -41,22 +58,37 @@ public class ReservationServicio {
             }
         }
     }
+    /**
+    * para actualizar la reserva si hay alguna modificación
+    */
     public Reservation update(Reservation reservation){
         if(reservation.getIdReservation()!=null){
             Optional<Reservation> e= crud.getReservation(reservation.getIdReservation());
             if(!e.isEmpty()){
 
                 if(reservation.getStartDate()!=null){
+                    /**
+    * pone el inicio de la reserva
+    */
                     e.get().setStartDate(reservation.getStartDate());
                 }
                 if(reservation.getDevolutionDate()!=null){
                     e.get().setDevolutionDate(reservation.getDevolutionDate());
+                                       /**
+    * pone el final de la reserva
+    */
                 }
                 if(reservation.getStatus()!=null){
                     e.get().setStatus(reservation.getStatus());
+                                       /**
+    * verifica el estatus, completed o cancelles
+    */
                 }
                 crud.save(e.get());
                 return e.get();
+                       /**
+    * para guardarla
+    */
             }else{
                 return reservation;
             }
@@ -64,27 +96,42 @@ public class ReservationServicio {
             return reservation;
         }
     }
-
+                   /**
+    * para borrar la reserva
+    */
     public boolean deleteReservation(int reservationId) {
         Boolean aBoolean = getReservation(reservationId).map(reservation -> {
             crud.delete(reservation);
+                                     /**
+    * responde si la reserva ha sido borrada
+    */
             return true;
         }).orElse(false);
         return aBoolean;
     }
-
+                         /**
+    * reporte del estado de la reserva completed /cancelles
+    */
     public StatusReservas getReservationsStatusReport(){
+  
         List<Reservation>completed=crud.getReservationByStatus("completed");
         List<Reservation>cancelled=crud.getReservationByStatus("cancelled");
     return new StatusReservas(completed.size(), cancelled.size());
     }
     
     public List<Reservation> getReservationPeriod(String dateA, String dateB){
+
+                           /**
+    * duracion d ela reserva
+    */
         SimpleDateFormat parser=new SimpleDateFormat("yyyy-MM-dd");
         Date aDate= new Date();
         Date bDate= new Date();
         
        try {
+                           /**
+    * pconvierte la fecha
+    */
            aDate = parser.parse(dateA);
            bDate = parser.parse(dateB);
        }catch(ParseException evt){
@@ -97,7 +144,9 @@ public class ReservationServicio {
        } 
     
     }
-    
+                       /**
+    * cuenta clientes en un periodo de tiempo
+    */
     public List<ContadorClientes> getTopClients(){
         return crud.getTopClients();
     }
